81f6aca20010e7390f75eac7e402513ec68c167a
Couture <couturecome@gmail.com>
2024-12-22 16:45:24 +0800

    地址

--- main.py
+++ main.py
@@ -317,10 +317,10 @@ if __name__ == "__main__":
     if not os.path.exists(BACKUP_DIR):
         os.mkdir(BACKUP_DIR)
     parser = argparse.ArgumentParser()
-    parser.add_argument("github_token", help="github_token")
+    parser.add_argument("G_T", help="G_T")
     parser.add_argument("repo_name", help="repo_name")
     parser.add_argument(
         "--issue_number", help="issue_number", default=None, required=False
     )
     options = parser.parse_args()
-    main(options.github_token, options.repo_name, options.issue_number)
+    main(options.G_T, options.repo_name, options.issue_number)
