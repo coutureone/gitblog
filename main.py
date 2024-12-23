0c2651283e50c57333ad6044bdb7d6feedeb7e89
couture <couturecome@gmail.com>
2024-12-23 14:06:18 +0800

    about me add

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## [Gitblog](https://coutureone.github.io/gitblog/)
-My personal blog([About Me](https://github.com/coutureone/gitblog/issues/282)) using issues and GitHub Actions (随意转载，无需署名)
+My personal blog([About Me](https://github.com/coutureone/gitblog/issues/22)) using issues and GitHub Actions (随意转载，无需署名)
 # ![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
