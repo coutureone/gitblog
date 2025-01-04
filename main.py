3b07dca40675d4d753288903c93c867cc19a3f85
coutureone <couturecome@gmail.com>
2025-01-04 14:52:28 +0800

    fix

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """**<p align="center">[Couture's Blog](https://blog.coutures.top)</p>**
+MD_HEAD = """**<p align="center">[Couture's Blog](https://blog.coutureone.top)</p>**
 ====
 
-**<p align="center">用于记录一些琐碎</p>**
+**<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
 
 
 ## 联系方式
 - Twitter：[@coutureone](https://twitter.com/coutureone)
-- Telegram：[@Coutureone](https://t.me/Coutureone)
-- Email：[couturecome@gmail.com](mailto:couturecome@gmail.com)
+- Telegram：[@coutureone](https://t.me/coutureone)
+- Email：[couturecome@gmail.com](mailto:couturecome@gmail.@163.com)
 - Blog：[https://blog.coutures.top](https://blog.coutures.top)
 - RSS：[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
