b9e94279992d317275431f90e537b81d5a0f9e43
coutureone <couturecome@gmail.com>
2025-01-04 15:21:50 +0800

    fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from marko.ext.gfm import gfm as marko
 MD_HEAD = """**<p align="center">[Couture's Blog](https://blog.coutureone.top)</p>**
 ====
 
-**<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
+**<p align="center">用于记录一些琐碎</p>**
 
 
 ## 联系方式
